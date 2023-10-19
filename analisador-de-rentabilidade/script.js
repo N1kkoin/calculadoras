575bc8d533d4cd419737f408f297e2a0650f5a49
NICOLE HEGUY <nikkonh@hotmail.com>
2023-10-19 20:26:22 -0300

    add taxa selic

---
+++ analisador-de-rentabilidade/script.js
@@ -0,0 +1,22 @@
+  // Função para buscar e exibir a taxa SELIC mais recente
+  function fetchSelicValue() {
+    fetch("https://api.bcb.gov.br/dados/serie/bcdata.sgs.1178/dados?formato=json")
+        .then(response => response.json())
+        .then(data => {
+            if (data.length > 0) {
+                // Reverte a ordem dos dados e pega o primeiro valor para obter o mais recente
+                data.reverse();
+                const selicValue = data[0].valor;
+                document.getElementById("selicValue").textContent = selicValue;
+            } else {
+                document.getElementById("selicValue").textContent = "Valores da taxa SELIC não encontrados.";
+            }
+        })
+        .catch(error => {
+            console.error("Erro ao buscar a taxa SELIC: " + error);
+            document.getElementById("selicValue").textContent = "Erro ao buscar a taxa SELIC.";
+        });
+}
+
+// Chama a função para buscar a taxa SELIC mais recente
+fetchSelicValue();
