255ae50d79fbfa6588bd3b51db83b4b671611403
N1kkoin <nikkonh@hotmail.com>
2023-10-20 01:17:52 -0300

    add conta luro final

--- analisador-de-rentabilidade/script.js
+++ analisador-de-rentabilidade/script.js
@@ -1,3 +1,6 @@
+let custoTotal = 0;
+
+
 
 // TAXA SELIC ---------------------------------------------------
 
@@ -65,46 +68,177 @@ document.querySelector('.datavenda').addEventListener('change', calcularDiferenc
 
 //  TEMPO (MESES) ----------------------------------------------------------------------------------------------------
 
- // Função para calcular a diferença em meses entre duas datas
- function calcularDiferencaMeses() {
+// Função para calcular a diferença em meses entre duas datas
+function calcularDiferencaMeses() {
     const dataCompra = new Date(document.querySelector('.datacompra').value);
     const dataVenda = new Date(document.querySelector('.datavenda').value);
 
     const diferencaMeses = (dataVenda.getMonth() + 1) + (dataVenda.getFullYear() - dataCompra.getFullYear()) * 12 - (dataCompra.getMonth() + 1);
 
     document.getElementById('tempomeses').textContent = diferencaMeses + ' meses';
-  }
+}
 
-  // Adicionar um evento onchange para os campos de data
-  document.querySelector('.datacompra').addEventListener('change', calcularDiferencaMeses);
-  document.querySelector('.datavenda').addEventListener('change', calcularDiferencaMeses);
+// Adicionar um evento onchange para os campos de data
+document.querySelector('.datacompra').addEventListener('change', calcularDiferencaMeses);
+document.querySelector('.datavenda').addEventListener('change', calcularDiferencaMeses);
 
 
-  //PLACEHOLDER TEMPO DIA E MESES ------------------------------------------------------------------------------------------------------------------------------------
+//PLACEHOLDER TEMPO DIA E MESES ------------------------------------------------------------------------------------------------------------------------------------
 
-  // Função para calcular a diferença em meses entre duas datas
-  function calcularDiferencaMeses() {
+// Função para calcular a diferença em meses entre duas datas
+function calcularDiferencaMeses() {
     const dataCompra = new Date(document.querySelector('.datacompra').value);
     const dataVenda = new Date(document.querySelector('.datavenda').value);
     const tempodiasSpan = document.getElementById('tempodias');
     const tempomesesSpan = document.getElementById('tempomeses');
 
     if (!isNaN(dataCompra) && !isNaN(dataVenda)) {
-      const diferencaDias = Math.abs((dataVenda - dataCompra) / (1000 * 60 * 60 * 24));
-      const diferencaMeses = (dataVenda.getMonth() + 1) + (dataVenda.getFullYear() - dataCompra.getFullYear()) * 12 - (dataCompra.getMonth() + 1);
+        const diferencaDias = Math.abs((dataVenda - dataCompra) / (1000 * 60 * 60 * 24));
+        const diferencaMeses = (dataVenda.getMonth() + 1) + (dataVenda.getFullYear() - dataCompra.getFullYear()) * 12 - (dataCompra.getMonth() + 1);
 
-      tempodiasSpan.textContent = diferencaDias + ' dias';
-      tempomesesSpan.textContent = diferencaMeses + ' meses';
+        tempodiasSpan.textContent = diferencaDias + ' dias';
+        tempomesesSpan.textContent = diferencaMeses + ' meses';
     } else {
-      tempodiasSpan.textContent = 'Tempo em dias';
-      tempomesesSpan.textContent = 'Tempo em meses';
+        tempodiasSpan.textContent = 'Tempo em dias';
+        tempomesesSpan.textContent = 'Tempo em meses';
     }
-  }
+}
 
-  // Adicionar um evento onchange para os campos de data
-  document.querySelector('.datacompra').addEventListener('change', calcularDiferencaMeses);
-  document.querySelector('.datavenda').addEventListener('change', calcularDiferencaMeses);
+// Adicionar um evento onchange para os campos de data
+document.querySelector('.datacompra').addEventListener('change', calcularDiferencaMeses);
+document.querySelector('.datavenda').addEventListener('change', calcularDiferencaMeses);
 
-  // Calcular a diferença de meses na carga da página
-  calcularDiferencaMeses();
+// Calcular a diferença de meses na carga da página
+calcularDiferencaMeses();
 
+
+
+
+
+
+//FORMATAÇÃO DINHEIRO R$ ----------------------------------------------------------------------------------------------------------
+
+// Adicionar eventos de entrada aos campos de valor pago e custos
+const valorPagoInput = document.querySelector('.valorpago');
+valorPagoInput.addEventListener('input', () => {
+    formatInputCurrency(valorPagoInput);
+    calcularCustoTotal();
+});
+
+const custosInput = document.querySelector('.custos');
+custosInput.addEventListener('input', () => {
+    formatInputCurrency(custosInput);
+    calcularCustoTotal();
+});
+
+const custosvalorVendaInput = document.querySelector('.valorvenda');
+custosvalorVendaInput.addEventListener('input', () => {
+    formatInputCurrency(custosvalorVendaInput);
+    calcularCustoTotal();
+});
+
+const deducoesInput = document.querySelector('.deducoes');
+deducoesInput.addEventListener('input', () => {
+    formatInputCurrency(deducoesInput);
+    calcularCustoTotal();
+});
+
+
+// VALOR PAGO + CUSTOS = CUSTOTOTAL -----------------------------------------------------------------------------------------------------------------
+
+// Função para calcular o custo total
+function calcularCustoTotal() {
+    const valorPagoInput = document.querySelector('.valorpago');
+    const custosInput = document.querySelector('.custos');
+    const custoTotalElement = document.getElementById('custototal');
+
+    // Obtém os valores formatados dos campos de entrada
+    const valorPagoFormatted = valorPagoInput.value || '0'; // Se estiver vazio, considera como zero
+    const custosFormatted = custosInput.value || '0'; // Se estiver vazio, considera como zero
+
+    // Converte os valores formatados em números
+    const valorPago = parseCurrency(valorPagoFormatted);
+    const custos = parseCurrency(custosFormatted);
+
+    custoTotal = valorPago + custos; // Armazena o valor do custo total na variável global
+
+    // Exibe o custo total formatado
+    custoTotalElement.textContent = formatCurrency(custoTotal);
+
+    // Chama a função para recalcular o lucro final
+    calcularLucroFinal();
+}
+
+// Função para analisar um valor monetário formatado (R$) e retornar um número
+function parseCurrency(currencyString) {
+    // Remove o "R$" e quaisquer separadores de milhares
+    const numericString = currencyString.replace(/\D/g, '');
+
+    // Converte a string numérica para um número de ponto flutuante
+    return parseFloat(numericString) / 100;
+}
+
+// Função para formatar um número como moeda brasileira (R$)
+function formatCurrency(value) {
+    return value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2
+    });
+}
+
+// Função para formatar um campo de entrada como moeda brasileira (R$)
+function formatInputCurrency(input) {
+    // Remove todos os caracteres não numéricos
+    var value = input.value.replace(/\D/g, '');
+
+    // Converta o valor em número
+    var numericValue = parseFloat(value) / 100;
+
+    // Formate o valor como moeda brasileira (R$) com separadores de milhares
+    if (!isNaN(numericValue)) {
+        input.value = formatCurrency(numericValue);
+    }
+}
+
+// Calcular o custo total na carga da página
+calcularCustoTotal();
+
+
+
+
+// LUCRO FINAL ----------------------------------------------------------------------------------------------------
+ 
+
+// VALOR VENDA - DEDUÇÕES - CUSTO TOTAL = LUCRO FINAL
+function calcularLucroFinal() {
+    const valorVendaInput = document.querySelector('.valorvenda');
+    const deducoesInput = document.querySelector('.deducoes');
+    const lucroFinalElement = document.getElementById('lucrofinal');
+
+    // Obtém os valores formatados dos campos de entrada
+    const valorVendaFormatted = valorVendaInput.value || '0'; // Se estiver vazio, considera como zero
+    const deducoesFormatted = deducoesInput.value || '0'; // Se estiver vazio, considera como zero
+
+    // Converte os valores formatados em números
+    const valorVenda = parseCurrency(valorVendaFormatted);
+    const deducoes = parseCurrency(deducoesFormatted);
+
+    const lucroFinal = (valorVenda - deducoes - custoTotal).toFixed(2); // Inclui o custoTotal na fórmula e arredonda para 2 casas decimais
+
+  
+    // Adicione "R$" ao valor do lucro final
+    lucroFinalElement.textContent = "R$ " + formatCurrency(lucroFinal);
+}
+
+// Eventos para calcular o custo total e o lucro final
+document.querySelector('.valorpago').addEventListener('keyup', calcularCustoTotal);
+document.querySelector('.custos').addEventListener('keyup', calcularCustoTotal);
+document.querySelector('.valorvenda').addEventListener('keyup', calcularLucroFinal);
+document.querySelector('.deducoes').addEventListener('keyup', calcularLucroFinal);
+
+// Calcular o custo total na carga da página
+calcularCustoTotal();
+
+// Calcular o lucro final na carga da página
+calcularLucroFinal();
